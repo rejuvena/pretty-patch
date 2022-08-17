5c5299723ad3019c9977e7222cb5496703ba9092
Tomat <xxlennygamerxx@gmail.com>
2022-08-16 19:26:32 -0700

    Initial project files.

---
+++ PrettyPatch.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace PrettyPatch
+{
+	public class PrettyPatch : Mod
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pretty Patch
+
+Library mod for advanced code patching in a tModLoader environment.
